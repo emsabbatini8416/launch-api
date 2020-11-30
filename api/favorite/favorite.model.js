bef7b6baabb77fef360e28c513e59c47fdbac4b1
Emmanuel Sabbatini <emmanuel.sabbatini@gmail.com>
2020-11-30 17:37:41 -0300

    minor change

--- api/favorite/favorite.model.js
+++ api/favorite/favorite.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema =  mongoose.Schema;
 
 const favoriteSchema = new Schema({
-    fligthNumber: {
+    flightNumber: {
         type: Number
     },
     date: {
